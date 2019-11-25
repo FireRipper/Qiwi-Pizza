91fe1f0e0b201ade64cc6110b8f144a3837c2597
FireRipper <shabelnyk.ilya@gmail.com>
2019-11-25 17:42:42 +0200

    feat(CardItems): add new functions: handleChangeSelectItem, handleChangeInputNumber, updateTotalCost for change and display total cost product.

--- src/components/CardItems/CardItems.js
+++ src/components/CardItems/CardItems.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import QiwiPizzaServices from '../../services/qiwi-pizza-services'
-import { Button, Card, Col, InputNumber, Row, Select } from 'antd'
+import { Button, Card, Col, Input, InputNumber, Row, Select } from 'antd'
 import noPhoto from '../../images/no-photo/no-photo-2-var/no-photo-var2-570px.png'
 import './CardItems.css'
 
@@ -12,7 +12,42 @@ export default class CardItems extends Component {
     qiwiService = new QiwiPizzaServices()
 
     state = {
-        pizzaList: this.qiwiService.getAllPizza()
+        pizzaList: this.qiwiService.getAllPizza(),
+        numberValue: 1,
+        totalCost: 0,
+        oldCost: 0,
+        selectValue: 'Выберите...'
+    }
+
+    handleChangeSelectItem = (id, value) => {
+        this.setState({
+            totalCost: value,
+            oldCost: value
+        })
+    }
+
+    handleChangeInputNumber = (value) => {
+        this.updateTotalCost(value)
+
+        this.setState({
+            numberValue: value
+        })
+    }
+
+    updateTotalCost = (val) => {
+        this.setState(({numberValue, totalCost, oldCost}) => {
+            if (val > numberValue) {
+                const newCost = oldCost * val
+                return {
+                    totalCost: newCost
+                }
+            } else {
+                const result = totalCost - oldCost
+                return {
+                    totalCost: result
+                }
+            }
+        })
     }
 
     renderItems(arr) {
@@ -23,7 +58,8 @@ export default class CardItems extends Component {
                             largeDesc, largeCost
                         }) => {
             return (
-                <Col xs={{span: 20}} sm={{ span: 9, offset: 1 }} md={{ span: 9, offset: 1 }} lg={{ span: 6, offset: 1 }} key={id + name}
+                <Col xs={{ span: 20 }} sm={{ span: 9, offset: 1 }} md={{ span: 9, offset: 1 }}
+                     lg={{ span: 6, offset: 1 }} key={id}
                      className='card-items'>
                     <Card hoverable
                           cover={<img src={noPhoto} alt={name} />}>
@@ -33,7 +69,9 @@ export default class CardItems extends Component {
                                 <strong>Выберите вес:</strong>
                             </Col>
                             <Col span={24}>
-                                <Select defaultValue={smallCost} className='card-items--select'>
+                                <Select defaultValue={this.state.selectValue}
+                                        onChange={(value) => this.handleChangeSelectItem(id, value)}
+                                        className='card-items--select'>
                                     <Option
                                         value={smallCost}
                                     >
@@ -61,15 +99,21 @@ export default class CardItems extends Component {
                                 <strong>Количество: </strong>
                             </Col>
                             <Col span={24}>
-                                <InputNumber min={1} defaultValue={1} max={50} className='card-items-input--number'/>
+                                <InputNumber
+                                    min={1}
+                                    defaultValue={this.state.numberValue}
+                                    max={50}
+                                    className='card-items-input--number'
+                                    onChange={this.handleChangeInputNumber}
+                                />
                             </Col>
                         </Row>
-                        <Row type='flex' align='middle' justify='center'>
-                            <Col xs={24} md={12}>
-                                <span>Цена:</span>
-                                <strong> {smallCost} грн.</strong>
+                        <Row type='flex' align='middle'>
+                            <Col xs={24}>
+                                <strong>Итоговая цена:</strong>
+                                <Input value={`${this.state.totalCost} грн.`} />
                             </Col>
-                            <Col xs={24} md={12}>
+                            <Col xs={24} className='card-items--button'>
                                 <Button type='primary'>Заказать</Button>
                             </Col>
                         </Row>
