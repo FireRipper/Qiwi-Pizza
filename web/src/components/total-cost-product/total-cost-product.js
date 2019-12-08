6c20193c8497f0a72f17cbf5fc0bab410fba4b3e
Illia <shabelnyk.ilya@gmail.com>
2019-12-08 16:46:46 +0200

    fix: update total-cost-product.js

--- web/src/components/total-cost-product/total-cost-product.js
+++ web/src/components/total-cost-product/total-cost-product.js
@@ -4,9 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 import { connect } from 'react-redux'
 
-const TotalCostProduct = (props) => {
-
-    const { totalCost } = props
+const TotalCostProduct = ({ totalCost }) => {
 
     return (
         <Row type='flex' align='middle'>
