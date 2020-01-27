aefbae33763d708ef69fdbf17a7d73fec09afde9
FireRipper <shabelnyk.ilya@gmail.com>
2020-01-27 14:00:08 +0200

    feat(change in app.js): add new markup for display all content(left-sidebar, header, content and etc.), using hook - useState for change state(collapse: true or false) for toggle left-sidebar on tablet and another devices(width must be < 768px)

--- web/src/components/app/app.js
+++ web/src/components/app/app.js
@@ -1,32 +1,37 @@
-import React, { Fragment } from 'react'
-import { Row } from 'antd'
+import React, { useState } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import Header from '../header'
-import Footer from '../footer'
-import Home from '../pages/home-page'
 import About from '../pages/about-page'
-import Contact from '../pages/contact-page'
-import PageNotFound from '../pages/404-page'
 import Delivery from '../pages/delivery-page'
+import PageNotFound from '../pages/404-page'
+import LeftSidebar from '../left-sidebar'
+import Contact from '../pages/contact-page'
+import Header from '../header'
+import ContainerContent from '../container-content'
+import ProductMenu from '../product-menu'
 
 import './app.css'
 
 const App = () => {
+    const [collapse, setCollapse] = useState(true)
+
+    const handleChange = () => setCollapse((prevState) => !prevState)
+
     return (
-        <Fragment>
-            <Header />
-            <Row className="app-content">
+        <div id="app-wrapper" className={collapse ? 'collapse' : null}>
+            <LeftSidebar />
+            <div className="main" role={'main'}>
+                <Header handleChange={handleChange} />
                 <Switch>
-                    <Route path={'/'} exact component={Home} />
+                    <Route path={'/'} exact render={() => <ContainerContent content={<ProductMenu />} />} />
                     <Route path={'/about'} component={About} />
-                    <Route path={'/contact'} component={Contact} />
                     <Route path={'/delivery'} component={Delivery} />
+                    <Route path={'/contact'} component={Contact} />
+
                     <Route render={() => <PageNotFound />} />
                 </Switch>
-            </Row>
-            <Footer />
-        </Fragment>
+            </div>
+        </div>
     )
 }
 
